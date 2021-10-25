59c14cb468b8a323426456604174f7fb2e9c73e9
clement fleur <clement.fleur@epitech.eu>
2021-10-25 18:11:56 +0200

    upload

--- QuickTest/CPE_duostumper_2_2019/inc/my(1).h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int launchbasic(char **av, int size);
-char *getword();
-int my_strlen(char *s);
-int findword(char *word, int size, char *arg);
-int my_getnbr(char *s, int base);
